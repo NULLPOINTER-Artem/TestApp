06310245493e32f1a96a38c1771146be0e4a80b6
Artem Orlov <universalism123@gmail.com>
2020-10-12 21:29:58 +0200

    Finished Test Application

--- src/actions/fetch.js
+++ src/actions/fetch.js
@@ -1,13 +1,13 @@
 import { FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS, FETCH_USERS_FAILURE } from './types'
-const axios = require('axios');
 
 export const onFetchPhotos = () => {
-    return async (dispatch) => {
+    return (dispatch) => {
         dispatch({ type: FETCH_USERS_REQUEST, payload: [] })
         try {
-            await axios.get('https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0')
-                .then(response => {
-                    dispatch({ type: FETCH_USERS_SUCCESS, payload: response.data });
+            fetch('https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0')
+                .then(response => response.json())
+                .then(responseJSON => {
+                    dispatch({ type: FETCH_USERS_SUCCESS, payload: responseJSON });
                 })
         } catch (error) {
             dispatch({ type: FETCH_USERS_FAILURE, payload: error });
